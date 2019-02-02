be0f24178728854723d03b769e8ef5fedde7b3e4
eddiehao1997 <45096749+eddiehao1997@users.noreply.github.com>
2019-02-02 22:49:20 +0800

    now this can replay message to Telegram user
    now fulfill basic requirement but no bonus point yet

--- ay18t2-smt203-asm-01.py
+++ ay18t2-smt203-asm-01.py
@@ -10,11 +10,8 @@ import json
 import datetime
 
 ########################################################################
-# global variables 
+# LTA methord URLs & Headers
 ########################################################################
-
-my_token = '797824642:AAFJicifpPwaeQ0FpdfliQOoAtoGV_cUISw' # put your secret Telegram token here 
-url_base = 'https://api.telegram.org/bot{}/'.format(my_token)
 
 url_busArrival = 'http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2'
 # if you read page 6 of the LTA data mall, notice that ALL requests to the data 
@@ -25,11 +22,13 @@ headers = {
 	'accept': 'application/json'
 }
 
-chat_id = '797824642' # please type in your Telegram user chat id here  
+########################################################################
+# Telegram method URLs & info
+########################################################################
 
-########################################################################
-# Telegram method URLs
-########################################################################
+my_token = '797824642:AAFJicifpPwaeQ0FpdfliQOoAtoGV_cUISw' # put your secret Telegram token here 
+url_base = 'https://api.telegram.org/bot{}/'.format(my_token)
+chat_id = '797824642' # please type in your Telegram user chat id here  
 
 url_getUpdates = '{}getupdates'.format(url_base)
 url_sendMsg = '{}sendMessage'.format(url_base)
@@ -40,16 +39,92 @@ url_sendMsg = '{}sendMessage'.format(url_base)
 # some suggested functions are below as a guide; 
 # however, you are free to change any of the functions below 
 
+########################################################################
+# Telegram listen and relpy functions
+########################################################################
+
 def send_welcome(chat_id):
 	# write code here 
-	bus_msg = get_busarrival_api(60141)
-
 	return
+
+def process_msg(msg_dict):
+	"""This function is used to process a 'message' dictionary in the msg_detail 
+	and send respond to the user according to 'text' \n This function doesn't return any value
+	"""
+	# Declaring text content
+	text = ['', '']
+	msg = ''
+
+	# Geting message details from msg_dict 
+	sender_id = msg_dict['from']['id']
+	text = msg_dict['text'].split('\n')
 	
-def listen_and_reply(chat_id):
-	# write code here 
+	# Analysing 'text' content 
+	try:
+		msg_type = msg_dict['entities'][0]['type'] 
+	except KeyError:
+		try: 
+			msg = get_busarrival_info(text[0], text[1])
+		except IndexError:
+			msg = get_busarrival_info(text[0]) 
+
+	# constructing parameter for the Telegram API request
+	sendMessage_para = {
+		'chat_id' : sender_id,
+		'text' : msg,
+		'parse_mode' : 'HTML'
+	}
+
+	r = requests.post(url = url_sendMsg, params = sendMessage_para)
+
+	print(msg)
+
+	return 
+
+def listen_and_record():
+	"""Getting update of unread message from Telegram API then process each message. 
+	This function also rewrite the 'offset.txt' with the update_id of the latest message
+	"""
+
+	# import 'offset' number from 'offset.txt' 
+	with open('offset.txt') as f:
+		offset_num_origion = int(f.readline())
+	offset_num = offset_num_origion
+
+	# constructing parameter for the Telegram API request
+	getUpdates_para = {
+		'offset' : offset_num,
+		'limit' : '',
+		'timeout' : '',
+		'allowed_updates' : ''
+	}
+
+	# Getting response from Telegram API
+	r = requests.get(url = url_getUpdates, params = getUpdates_para)
+	unread_msg = r.json()['result']
+
+	# Printing Unread messages
+	print(json.dumps(unread_msg, sort_keys=True, indent=4))
+
+	# Processing each item in the 'result' 
+	for msg_detail in unread_msg:
+		# Update latest 'update_id' to offset_num for later use
+		if msg_detail['update_id'] >= offset_num :
+			offset_num = msg_detail['update_id'] + 1
+		# process message
+		process_msg(msg_detail['message'])
+	
+	# writing latest 'update_id' into 'offset.txt'
+	if offset_num != offset_num_origion:
+		with open('offset.txt', 'w') as f:
+			f.write(str(offset_num))
+
 	return 
 	
+########################################################################
+# LTA bus info construction functions 
+########################################################################
+
 def compute_busarrival(estimated_arrival):
 	"""
 	Returning an integer
@@ -92,7 +167,7 @@ def construct_msg(bus_dict):
 	msg =''
 	if bus_dict != {} :
 		for bus_No, time_list in bus_dict.items():
-			msg += '{:8}'.format('No.' + bus_No + ':') # bus service No.
+			msg += '<b>' + '{:8}'.format('No.' + bus_No + ':') + '</b>' # bus service No.
 			for time_min in time_list:
 				if time_min == -1 :
 					msg += 'N.A.  '
@@ -102,11 +177,11 @@ def construct_msg(bus_dict):
 					msg += '{:6}'.format('Arr')
 			msg += '\n'
 	else:
-		msg = "Too late alr,no bus services avaliable :("
+		msg = "Too late, no bus services avaliable for this station :("
 	
 	return msg
 
-def get_busarrival_dict(bus_list):
+def construct_busarrival_dict(bus_list):
 	"""
 	Return a dictionary of incoming bus in the following form
 	incoming_bus{
@@ -132,14 +207,16 @@ def get_busarrival_dict(bus_list):
 			incoming_bus[bus['ServiceNo']].append(-1)
 	return (incoming_bus)
 
-def get_busarrival_api(bus_stop_code, bus_svc_no = ''):
-	"""returning a message which is designed for Telegram
-	with user's input of bus_stop No. and bus_svc No.
-	In the form of 'bus_No.: eta1 eta2 eta3'
-	No.123: Arr   09min 15min 
-	No.12:  03min 14min 21min
-	No.143: Arr   09min 15min 
-	No.12:  03min 14min 21min
+def get_busarrival_info(bus_stop_code, bus_svc_no = ''):
+	"""returning a message which is designed for Telegram with user's input of  
+		bus_stop_code(string), and 
+		bus_svc_no(string)
+	In the form of: \n
+	bus_No: eta1  eta2  eta3  \n
+	No.123: Arr   09min 15min \n
+	No.12:  03min 14min 21min \n
+	No.143: Arr   09min 15min \n
+	No.12:  03min 14min 21min \n
 	"""
 	msg = ''
 	params = {
@@ -151,16 +228,16 @@ def get_busarrival_api(bus_stop_code, bus_svc_no = ''):
 	r = requests.get(url=url_busArrival, headers=headers, params=params)
 
 	# Choosing bus services info 
-	bus_srvs = get_busarrival_dict(r.json()['Services'])
-	# incoming_buses = r.json()['Services']
-	# print(json.dumps(incoming_buses, sort_keys=True, indent=4))
+	bus_srvs = construct_busarrival_dict(r.json()['Services'])
+
+	# Printing incoming Bus info
+	print(json.dumps(r.json(), sort_keys=True, indent=4))
 
 	# constructing message designed for Telegram
 	msg = construct_msg(bus_srvs)
 	# print(msg)
 	return msg 
-	
 
-# send_welcome(chat_id)	
-# listen_and_reply(chat_id)
-print(get_busarrival_api(60141))
+listen_and_record()
+
+# print(get_busarrival_info(283457))
